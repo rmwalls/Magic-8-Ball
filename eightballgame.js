512c0e20532ee3b4420145111f649b7e8d451718
ZelleM <groovyrlm@gmail.com>
2019-05-19 14:05:10 -0400

    Trying to get the text in the triangle

---
+++ eightballgame.js
@@ -0,0 +1,21 @@
+const answers = ["The answer is NO", "Outlook is good", "Why not?", "Emphatically YES!", "The future is unwritten"];
+console.log(answers);
+
+const myButton = document.querySelector("#a-button");
+console.log(myButton);
+
+myButton.addEventListener('click', () => {
+    const input = document.querySelector("#question");
+    console.log(input);
+    const inputVal = input.value;
+    const isQuestion = inputVal.includes("?");
+    if (inputVal.trim() !== "" && isQuestion) {
+        input.value = "";
+        const randomNum = Math.floor(Math.random() * answers.length);
+        const reply = answers[randomNum];
+        const message = document.querySelector("#ball-text");
+        message.innerText = reply;
+    } else {
+        alert("Please enter a question");
+    }
+});
