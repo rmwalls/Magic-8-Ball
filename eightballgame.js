9b972f50a850b3ec2dc9e0569ffe73e23417f8f8
ZelleM <groovyrlm@gmail.com>
2019-09-22 21:02:46 -0400

    Adjust text spacing, better now

--- eightballgame.js
+++ eightballgame.js
@@ -1,12 +1,12 @@
-const answers = ["The answer is NO", "Ask your mother", "Outlook is good", "Why not?", "Emphatically YES!", "The future is unwritten", "How should I know?", "Ask your father"];
+const answers = ["The answer is NO", "Ask your mother", "Outlook is good", "Well, why not?", "Emphatically YES!", "The future is unwritten", "How should I know?", "Ask your father"];
 console.log(answers);
 
 const myButton = document.querySelector("#a-button");
-console.log(myButton);
+//console.log(myButton);
 
 myButton.addEventListener('click', () => {
     const input = document.querySelector("#question");
-    console.log(input);
+    //console.log(input);
     const inputVal = input.value;
     const isQuestion = inputVal.includes("?");
     if (inputVal.trim() !== "" && isQuestion) {
